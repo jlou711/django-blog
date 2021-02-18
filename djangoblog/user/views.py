7f47aa4a906ddb5c10b3096e0b024103818ebd48
Jamie <jlou711@googlemail.com>
2021-02-18 17:48:56 +0000

    part 9

--- djangoblog/user/views.py
+++ djangoblog/user/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
-from .forms import UserRegisterForm
+from .forms import UserRegisterForm, UserUpdateForm, ProfileUpdateForm
 
 # Create your views here.
 
@@ -20,4 +20,16 @@ def register(request):
 # This is called a decorator, it adds functionality to an existing function, can be applied to classes too
 @login_required
 def profile(request):
-    return render(request, 'user/profile.html', {'title': "Profile"})
+    if request.method == 'POST':
+        u_form = UserUpdateForm(request.POST, instance=request.user)
+        p_form = ProfileUpdateForm(request.POST,request.FILES, instance=request.user.profile)
+        if u_form.is_valid() and p_form.is_valid():
+            u_form.save()
+            p_form.save()
+            messages.success(request, f'Account has been updated')
+            return redirect('profile')
+    else:
+        u_form = UserUpdateForm(instance=request.user)
+        p_form = ProfileUpdateForm(instance=request.user.profile)
+
+    return render(request, 'user/profile.html', {'title': "Profile", 'u_form': u_form, 'p_form':p_form})
